441321af56dd8ae834758beee62e964e64bf0727
Mihir Sarwade <mihirsarwade32@gmail.com>
2023-10-27 14:16:21 +0530

    Effect added

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-
-
-// const changeMode = () => {
-//     let mybody = document.body;
-//     mybody.classList.toggle('mydark');
-// }
-
-
-
-
 let toggle = document.getElementById("mode");
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('mydark');
@@ -15,12 +5,22 @@ toggle.addEventListener('click', () => {
 
 
 const onloadPage = () =>{
-    document.getElementById('main-body').style.opacity = '0.5';
-    // document.getElementById('coupen').style.opacity = '1';
+    document.getElementById('title-id').style.opacity = '0.1';
+    document.getElementById('topslider-id').style.opacity = '0.1';
+    document.getElementById('locate').style.opacity = '0.1';
+    document.getElementById('weather').style.opacity = '0.1';
+    document.getElementById('mode').style.opacity = '0.1';
     document.getElementById('coupen').style.visibility = 'visible';
+    
 }
+
 
 const closeCoupen = () =>{
     document.getElementById('coupen').style.visibility = 'hidden';
-    document.getElementById('main-body').style.opacity = '1';
+    document.getElementById('title-id').style.opacity = '1';
+    document.getElementById('topslider-id').style.opacity = '1';
+    document.getElementById('locate').style.opacity = '1';
+    document.getElementById('weather').style.opacity = '1';
+    document.getElementById('mode').style.opacity = '1';
+    
 }
