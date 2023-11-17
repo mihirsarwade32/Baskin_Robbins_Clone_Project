9d4d154ef1a2939fc121064fe907bd213388e471
Mihir Sarwade <mihirsarwade32@gmail.com>
2023-11-17 17:42:19 +0530

    added code

--- node/baskinapi/app.js
+++ node/baskinapi/app.js
@@ -4,3 +4,47 @@ let port = 3299
 let mongo = require('mongodb');
 let bodyParser = require('body-parser');
 let cors = require('cors');
+let {dbConnect, getData} = require('./controller/dbController');
+
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
+
+app.get('/',(req, res) => {
+    res.send ('Hi from Json')
+})
+
+
+app.get('/locations', async(req, res) => {
+    let query = {};
+    let collection = "locations";
+    let output = await getData(collection, query);
+    res.send (output)
+})
+
+
+
+app.listen(port, (err) => {
+    dbConnect();
+    if (err) throw err;
+    console.log(`Sever is running on port ${port}`)
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ node/baskinapi/controller/dbController.js
@@ -0,0 +1,31 @@
+let mongo = require ('mongodb');
+let {MongoClient} = require('mongodb');
+let mongoUrl = "mongodb+srv://mihir:yA4Mvzo2GqURr4Zk@cluster0.orpn7yf.mongodb.net/?retryWrites=true&w=majority";
+let client = new MongoClient(mongoUrl);
+
+async function dbConnect(){
+    await client.connect();
+}
+
+let db = client.db('stores');
+
+async function getData(ColName,query){
+    let output = [];
+    try{
+        const cursor = db.collection(colName).find(query);
+        for await (const data of cursor){
+            output.push(data)
+        }
+        cursor.close();
+    }catch(err){
+        output.push({"Error" : "Error in getting data"})
+    }
+    return output
+}
+
+
+
+module.exports = {
+    dbConnect,
+    getData
+}
